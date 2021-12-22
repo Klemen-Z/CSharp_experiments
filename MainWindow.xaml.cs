2ccd763ca3a05a03ae9703f25506507f86d39615
Klemen-Z <klemenzmahar@gmail.com>
2021-12-22 16:25:45 +0100

    help

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace CSharp_experiments
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
